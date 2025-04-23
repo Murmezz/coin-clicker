155b52b67c237e51c61cad2e2c3cb0a538c134bc
Murmezz <lovison4ik@gmail.com>
2025-04-23 20:49:13 +0300

    Update animations.js

--- public/src/animations.js
+++ public/src/animations.js
@@ -1,3 +1,12 @@
+export const createDentEffect = (parentElement, clickX, clickY) => {
+    const dent = document.createElement('div');
+    dent.className = 'dent-effect';
+    dent.style.left = `${clickX}px`;
+    dent.style.top = `${clickY}px`;
+    parentElement.appendChild(dent);
+    setTimeout(() => dent.remove(), 300); // Уменьшили время
+};
+
 export const createPlusOneEffect = (startX, startY) => {
     const effect = document.createElement('div');
     effect.className = 'plus-one-effect';
@@ -17,4 +26,25 @@ export const createPlusOneEffect = (startX, startY) => {
     
     document.body.appendChild(effect);
     setTimeout(() => effect.remove(), 1000);
+};
+
+export const tiltCoin = (element, clickX, clickY) => {
+    const rect = element.getBoundingClientRect();
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    
+    // Уменьшенный наклон (15 градусов вместо 20)
+    const tiltX = ((clickX - centerX) / centerX) * 15;
+    const tiltY = ((clickY - centerY) / centerY) * -15;
+    
+    element.style.transform = `
+        perspective(800px)
+        rotateX(${tiltY}deg)
+        rotateY(${tiltX}deg)
+        scale(0.95)
+    `;
+    
+    setTimeout(() => {
+        element.style.transform = '';
+    }, 250); // Укороченная анимация
 };
