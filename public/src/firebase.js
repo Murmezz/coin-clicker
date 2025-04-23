b76a155949347c131d246ef5f7b89056dbc82cbe
Murmezz <lovison4ik@gmail.com>
2025-04-23 09:20:17 +0300

    Create src/firebase.js

---
+++ public/src/firebase.js
@@ -0,0 +1,17 @@
+// Firebase configuration and initialization
+const firebaseConfig = {
+    apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
+    authDomain: "fastcoin-7db18.firebaseapp.com",
+    databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fastcoin-7db18",
+    storageBucket: "fastcoin-7db18.appspot.com",
+    messagingSenderId: "1024804439259",
+    appId: "1:1024804439259:web:351a470a824712c494f8fe"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
+
+export { db, auth };
