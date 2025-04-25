7d719fb9acfa55ca9fd83cad21de83b4991b3c75
Murmezz <lovison4ik@gmail.com>
2025-04-25 17:39:59 +0300

    Test firebase.js

--- public/src/firebase.js
+++ public/src/firebase.js
@@ -1,20 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
-    authDomain: "fastcoin-7db18.firebaseapp.com",
-    databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fastcoin-7db18",
-    storageBucket: "fastcoin-7db18.appspot.com",
-    messagingSenderId: "1024804439259",
-    appId: "1:1024804439259:web:351a470a824712c494f8fe"
+  apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
+  authDomain: "fastcoin-7db18.firebaseapp.com",
+  databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fastcoin-7db18",
+  storageBucket: "fastcoin-7db18.appspot.com",
+  messagingSenderId: "1024804439259",
+  appId: "1:1024804439259:web:351a470a824712c494f8fe"
 };
 
-// Инициализируем Firebase
 const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-const auth = firebase.auth();
 
-// Добавляем анонимную авторизацию
+// Авторизация перед использованием
+const auth = firebase.auth();
+const db = firebase.database();
+
+// Анонимная авторизация
 auth.signInAnonymously()
-    .catch(error => console.error('Auth error:', error));
+  .then(() => console.log("Firebase auth success"))
+  .catch((error) => console.error("Firebase auth error:", error));
 
 export { db, auth };
