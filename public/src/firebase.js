869ed55c0163ca9c79c68c3d22a94ad3832aaa96
Murmezz <lovison4ik@gmail.com>
2025-04-25 07:05:12 +0300

    Update firebase.js

--- public/src/firebase.js
+++ public/src/firebase.js
@@ -1,15 +1,17 @@
+// Firebase configuration and initialization
 const firebaseConfig = {
-  apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
-  authDomain: "fastcoin-7db18.firebaseapp.com",
-  databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fastcoin-7db18",
-  storageBucket: "fastcoin-7db18.appspot.com",
-  messagingSenderId: "1024804439259",
-  appId: "1:1024804439259:web:351a470a824712c494f8fe"
+    apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
+    authDomain: "fastcoin-7db18.firebaseapp.com",
+    databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fastcoin-7db18",
+    storageBucket: "fastcoin-7db18.appspot.com",
+    messagingSenderId: "1024804439259",
+    appId: "1:1024804439259:web:351a470a824712c494f8fe"
 };
 
-// Инициализация Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-console.log("Firebase initialized!"); // Добавьте эту строку для проверки
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
+
+export { db, auth };
