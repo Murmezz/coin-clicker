c5684130eeebcde1fce43a0bf20277e953cdfdeb
Murmezz <lovison4ik@gmail.com>
2025-04-27 13:23:56 +0300

    Basic firebase.js

--- public/src/firebase.js
+++ public/src/firebase.js
@@ -1,22 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app-compat.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-database-compat.js";
-import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth-compat.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
-  authDomain: "fastcoin-7db18.firebaseapp.com",
-  databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fastcoin-7db18",
-  storageBucket: "fastcoin-7db18.appspot.com",
-  messagingSenderId: "1024804439259",
-  appId: "1:1024804439259:web:351a470a824712c494f8fe"
+    apiKey: "AIzaSyBlB5mKpyKi2MVp2ZYqbE3kBc0VdmXr3Ik",
+    authDomain: "fastcoin-7db18.firebaseapp.com",
+    databaseURL: "https://fastcoin-7db18-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fastcoin-7db18",
+    storageBucket: "fastcoin-7db18.appspot.com",
+    messagingSenderId: "1024804439259",
+    appId: "1:1024804439259:web:351a470a824712c494f8fe"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app);
+// Инициализируем Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
 
-signInAnonymously(auth)
-  .catch((error) => console.error("Auth error:", error));
+// Добавляем анонимную авторизацию
+auth.signInAnonymously()
+    .catch(error => console.error('Auth error:', error));
 
 export { db, auth };
